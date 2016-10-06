96983f1178e3d84fd3769370dd6ec4f1b6a16b7b
masterbachman <masterbachman@gmail.com>
2016-10-05 17:28:13 -0700

    Count Occurance
    Needs to count occurrence of campGroup from CSV file.

--- csvReader.cpp
+++ csvReader.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
@@ -27,6 +28,21 @@ int main(){
 	cout << lines.size();
     file.close();
     
+
+
+    typedef std::tr1::unordered_map<int,unsigned int> CounterMap;
+	CounterMap counts;
+	for (int i = 0; i < lines.size(); ++i)
+	{
+   		CounterMap::iterator i(counts.find(lines[i]));
+   		if (i != counts.end()){
+      		i->second++;
+   		} else {
+      		counts[lines[i]] = 1;
+   		}
+	}
+
+
 	/*
 	string stringIn = "my,csv,,is 10233478,separated,by commas";
 	vector<std::string> commaSeparated(1);
