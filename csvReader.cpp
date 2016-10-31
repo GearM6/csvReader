75857672d84afbdd0dded46559b291228879b61a
Matt Marra <matt@mattmarra.com>
2016-10-31 13:52:28 -0700

    Added ReadMe and Gitignore
    That’s it

--- csvReader.cpp
+++ csvReader.cpp
@@ -7,35 +7,55 @@
 
 using namespace std;
 
+vector<string> catalog(string path){
+	vector<string> temp;
+	ifstream file;
+	string line;
+
+
+	file.open(path); //Open the file
+	if (file.fail()){   cout << "Failed to open file.\n";   return 0;}
+	else{
+		getline(file,line); //Receive the input
+		if(file.is_open()){ // if the file is open, proceed
+			while(file.good()){ // read all the good input from the file
+				getline(file, line); // add the current line from the file to the vector 
+				line.push_back(line);
+			}
+			cout << "Number of elements: " << temp.size() << endl; //Report the number of collected elements
+	    	file.close(); //Close the file
+	    	return temp; //return the vector
+		}
+		else{cout << "File not open, terminating program.\n";}
+    }
+}
+
 int main(){
+	enum gender {male, female};
+
+	struct person
+	{
+		string name;
+		gender pGender;
+		int groupNumber;
+
+	};
+
 	int campTotal;
 	int campGroup;
 	int campCapacity;
 	int maxCapacity;
-	int maxFemale;
-	int maxMale;
+	int maxFemale = maxCapacity/2;
+	int maxMale = maxCapacity/2;
 
 	vector<string> lines;
-	ifstream file;
-	string line, filePath;
+	string filePath;
 
 	cout << "Drag and drop the file you want to catalog: ";
 	cin >> filePath;
 	cout << endl;
-	file.open(filePath);
-	if (file.fail()){   cout << "Failed to open file.\n";   return 0;}
-	else{
-		getline(file,line);
-		if(file.is_open()){
-			while(file.good()){
-				getline(file, line);
-				lines.push_back(line);
-			}
-			cout << "Number of elements: " << lines.size() << endl;
-	    	file.close();
-		}
-		else{cout << "File not open, terminating program.\n";}
-    }
+
+	lines = catalog(filePath);
 
     map<string,string> grouping;
     for (int i = 0; i < lines.size(); ++i)
@@ -56,5 +76,16 @@ int main(){
     cout << "Enter the name you want to find: (type 'done' to quit)\n";
     cin.ignore(1000, '\n');
     getline(cin, input);
-    cout << input << " is in group: " << grouping[input] << endl;
+    while(cin.good()){
+    	if (input == "done" or input == "Done")
+    		{break;}
+    	else if (grouping[input] == ""){
+    		cout << "Person not found.\n";
+    	}
+    	else {
+    		cout << input << " is in group: " << grouping[input] << endl;    		
+    	}
+    	getline(cin, input);
+    }
+    
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+#CSV Reader
+This is a small project that allows you to query information from a .csv file. It was made with the original purpose of reading in information about campers at a camp, but feel free to make changes as you see fit.
+
+Licensed under MIT license.
